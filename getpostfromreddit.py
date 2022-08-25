c7c9dea7d95c751f23eb30d7074fc46f6e537036
Kishor <34562761+k1sh0r@users.noreply.github.com>
2022-08-25 11:54:07 +0530

    added zen

--- getpostfromreddit.py
+++ getpostfromreddit.py
@@ -54,3 +54,52 @@ def downloadMeme(category, count = 1):
     img_io.seek(0)
     return send_file(img_io, mimetype='image/jpeg')
     # return memebg
+
+def downloadZenMeme(category, count = 1):
+    reddit = praw.Reddit(client_id='PTELqQeemTWiZ-L61Q3REA', client_secret='M8hcW0t8L8W8y-zSnhaOr7ZYhUH_fg', user_agent='MemeBot')
+    if category == "hot":
+        hot_posts = reddit.subreddit('css_irl+ProgrammerHumor+programming+programming_memes+programmingmemes+programminghorror').hot()
+    elif category == "new":
+        hot_posts = reddit.subreddit('css_irl+ProgrammerHumor+programming+programming_memes+programmingmemes+programminghorror').new()
+
+    for post in hot_posts:
+        if(post.post_hint == 'image'):
+            count = count -1
+            if count <= 1:
+                memeurl = post.url
+                print('meme available')
+                break
+
+    # for post in hot_posts:
+    #     print(post.url + " " + post.post_hint)
+
+    print(memeurl)
+    memebg = Image.open('memebgzen.jpg')
+    memecontent = Image.open(BytesIO(requests.get(memeurl).content))
+    print(memecontent.size)
+
+    width, height = memecontent.size
+
+    if(width>=height):
+        scaleratio = 968/width
+        newwidth = round(scaleratio*width)
+        newheight = round(scaleratio*height)    
+        memecontent = memecontent.resize((newwidth,newheight))
+        memebg.paste(memecontent,(56,round(508-(newheight/2))))
+    else:
+        scaleratio = 824/height
+        newheight = round(scaleratio*height)
+        newwidth = round(scaleratio*width)
+        if(newwidth>904):
+            scaleratio = (968/width)
+            newwidth = round(scaleratio*width)
+            newheight = round(scaleratio*height)
+        memecontent = memecontent.resize((newwidth,newheight))
+        memebg.paste(memecontent,(round(540-(newwidth/2)),round(508-(newheight/2))))
+
+    # memebg.save('final.jpg')
+    img_io = BytesIO()
+    memebg.save(img_io, 'JPEG', quality=70)
+    img_io.seek(0)
+    return send_file(img_io, mimetype='image/jpeg')
+    # return memebg
--- main.py
+++ main.py
@@ -12,5 +12,14 @@ def dynamic_page():
     print(request.args.get('count', default = 1, type = int))
     return getpostfromreddit.downloadMeme("new",request.args.get('count', default = 1, type = int))
 
+@app.route('/zenhot')
+def hot_meme():
+    return getpostfromreddit.downloadZenMeme("hot",request.args.get('count', default = 1, type = int))
+
+@app.route('/zennew')
+def dynamic_page():
+    print(request.args.get('count', default = 1, type = int))
+    return getpostfromreddit.downloadZenMeme("new",request.args.get('count', default = 1, type = int))
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='8050', debug=True)
